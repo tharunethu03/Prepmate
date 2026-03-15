90c44766d12f7ffd9acab9b8b90cf2c2f5e367bf
tharunethu03 <tharunethu03@icloud.com>
2026-03-15 06:39:51 +0530

    fix: middleware export and next.config serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", "bcrypt"],
-  },
+  serverExternalPackages: ["@prisma/client", "bcrypt"],
 };
 
 export default nextConfig;
